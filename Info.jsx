a35911fd8837a1630a60605774e8635579e9f43e
AnkushDingankar <85779039+AnkushDingankar@users.noreply.github.com>
2021-06-26 21:50:26 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1> Javascript With react.js</h1>
+      <p>A basic web dev rect Js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
